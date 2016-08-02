cd374c1af6fd0ac89552431317f9df00647b6b1a
诚勇刘 <liuchengyong@iluoteng.com>
2016-08-02 18:27:49 +0800

    提交发布脚本

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ssh -i ~/.ssh/LTEST_rsa root@128.199.143.197 bash -c "'
+	cd /node/jingdongdad
+	git fetch --all
+	git reset --hard origin/master
+	source ~/.nvm/nvm.sh 
+	bash update.sh
+'"
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+npm run test			
+cp -r ./_output/* /dist/jingdongdad/
