dbc9d4c69d893a48157e72694325ab2507003c32
诚勇刘 <liuchengyong@iluoteng.com>
2016-08-05 09:42:56 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ssh -i ~/.ssh/LTEST_rsa root@128.199.143.197 bash -c "'
 	cd /node/jingdongdad
+	rm -r dist
+	rm -r _output
+	mkdir dist
+	mkdir _output
 	git fetch --all
 	git reset --hard origin/master
-	source ~/.nvm/nvm.sh 
-	bash update.sh
+	source ~/.nvm/nvm.sh
+	NODE_ENV=production bash update.sh
 '"
